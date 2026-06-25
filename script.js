f3435651fab8da39aa8b131cdcfedb3ead165ed5
vk56hhzgsn-star <vk56hhzgsn@privaterelay.appleid.com>
2026-06-25 22:31:56 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,32 @@ function unlockWebsite() {
         document.getElementById("lockScreen").style.display = "none";
         document.getElementById("mainContent").style.display = "block";
 
+        startEffects();
+
     } else {
 
         alert("Wrong password ❤️");
 
     }
+}
+
+/* 🌸 EFFECTS */
+function startEffects(){
+
+    setInterval(createPetal, 300);
+}
+
+/* 🌸 FALLING PETALS */
+function createPetal(){
+
+    const petal = document.createElement("div");
+    petal.classList.add("petal");
+    petal.innerHTML = "🌸";
+
+    petal.style.left = Math.random()*100 + "vw";
+    petal.style.animationDuration = (3+Math.random()*3)+"s";
+
+    document.body.appendChild(petal);
+
+    setTimeout(()=>{petal.remove();},6000);
 }
