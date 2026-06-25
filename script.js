3786dfcb2932099a345e6858fb8082a3e9ce3d99
vk56hhzgsn-star <vk56hhzgsn@privaterelay.appleid.com>
2026-06-26 00:18:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,33 @@ function unlockWebsite() {
 
     }
 }
+function createHeartBurst(){
+
+    for(let i=0;i<40;i++){
+
+        const heart =
+            document.createElement("div");
+
+        heart.classList.add("heart");
+
+        heart.innerHTML = "❤️";
+
+        heart.style.left =
+            Math.random()*100 + "vw";
+
+        heart.style.bottom =
+            "0px";
+
+        heart.style.animationDuration =
+            (1 + Math.random()*2) + "s";
+
+        document.body.appendChild(heart);
+
+        setTimeout(()=>{
+            heart.remove();
+        },3000);
+    }
+}
 
 /* 🌸 PETALS */
 function startEffects(){
