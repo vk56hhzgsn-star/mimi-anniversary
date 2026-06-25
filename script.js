67778ad7ef42d37e080006809175c4e23871fd3e
vk56hhzgsn-star <vk56hhzgsn@privaterelay.appleid.com>
2026-06-25 21:36:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function unlockWebsite() {
-    alert("Button Works!");
+
+    const password = document.getElementById("password").value;
+
+    if (password === "1225") {
+
+        document.getElementById("lockScreen").style.display = "none";
+        document.getElementById("mainContent").style.display = "block";
+
+        alert("Welcome Mimi ❤️");
+
+    } else {
+
+        alert("Wrong password baby ❤️");
+
+    }
 }
